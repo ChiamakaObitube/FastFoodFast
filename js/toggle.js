09da5af536041b45702d90ca3f854a72876bd5b2
chiamakaobitube <obitubechiamaka@gmail.com>
2019-04-19 05:26:44 +0100

    refactored some file in the UI

---
+++ js/toggle.js
@@ -0,0 +1,12 @@
+var modal1 = document.getElementById('id01');
+var modal2 = document.getElementById('id02');
+var loginModal = document.querySelector('.modal-content');
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal1 || event.target == modal2) {
+        modal1.style.display = "none";
+        modal2.style.display = "none";
+    }
+}
